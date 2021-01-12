908bf4c65de9d1137bcfe1380f8d3aa66ada38fe
quasar1533 <1029230641@qq.com>
2021-01-12 19:39:16 +0800

    version9

--- src/app1.js
+++ src/app1.js
@@ -1,69 +1,55 @@
 import "./app1.css";
-import $ from "jquery";
-import Model from "./base/Model.js";
-import View from "./base/View.js";
+import Vue from "vue";
 
 const localKey = "value"; // localStorage记录键名
 
-const m = new Model({
-  data: {
-    n: parseFloat(localStorage.getItem(localKey)) || 100,
-  },
-  update(data) {
-    Object.assign(this.data, data);
-    this.trigger("m:update");
-    localStorage.setItem(localKey, this.data.n);
-  },
-});
-
 const init = (container) => {
-  new View({
+  const m = {
+    get: () => {
+      return parseFloat(localStorage.getItem(localKey)) || 100;
+    },
+    set: (n) => {
+      localStorage.setItem(localKey, n);
+    },
+  };
+  new Vue({
     el: container,
-    data: m.data,
-    html: `
-      <div class="wrapper">
-        <div class="output">
-          <span id="number">{{n}}</span>
+    data: {
+      n: m.get(),
+    },
+    template: `
+      <section id="app1">
+        <div class="wrapper">
+          <div class="output">
+            <span id="number">{{n}}</span>
+          </div>
+          <div class="buttons">
+            <button @click="add">+1</button>
+            <button @click="minus">-1</button>
+            <button @click="multi">*2</button>
+            <button @click="divide">/2</button>
+          </div>
         </div>
-        <div class="buttons">
-          <button id="add1">+1</button>
-          <button id="minus1">-1</button>
-          <button id="multi2">*2</button>
-          <button id="divide2">/2</button>
-        </div>
-      </div>  
-      `,
-    render(data) {
-      const n = data.n;
-      if (this.el.children().length !== 0) this.el.empty();
-      $(this.html.replace("{{n}}", n)).appendTo(this.el);
+      </section> 
+    `,
+    methods: {
+      add() {
+        this.n += 1;
+      },
+      minus() {
+        this.n -= 1;
+      },
+      multi() {
+        this.n *= 2;
+      },
+      divide() {
+        this.n /= 2;
+      },
     },
-    events: {
-      "click #add1": "add1",
-      "click #minus1": "minus1",
-      "click #multi2": "multi2",
-      "click #divide2": "divide2",
-    },
-    autoBindEvents() {
-      for (let key in this.events) {
-        const value = this[this.events[key]];
-        const spaceIndex = key.indexOf(" ");
-        const eventType = key.slice(0, spaceIndex);
-        const selector = key.substring(spaceIndex + 1);
-        this.el.on(eventType, selector, value);
-      }
-    },
-    add1() {
-      m.update({ n: (m.data.n += 1) });
-    },
-    minus1() {
-      m.update({ n: (m.data.n -= 1) });
-    },
-    multi2() {
-      m.update({ n: (m.data.n *= 2) });
-    },
-    divide2() {
-      m.update({ n: (m.data.n /= 2) });
+    watch: {
+      n() {
+        m.set(this.n);
+      },
     },
   });
 };
--- src/app2.js
+++ src/app2.js
@@ -1,63 +1,42 @@
 import "./app2.css";
-import $ from "jquery";
-import Model from "./base/Model.js";
-import View from "./base/View.js";
+import Vue from "vue";
 
 const localKey = "app2.index"; // localStorage记录键名
 
-const m = new Model({
-  data: {
-    index: parseInt(localStorage.getItem(localKey)) || 0,
-  },
-  update(data) {
-    Object.assign(this.data, data);
-    this.trigger("m:update");
-    localStorage.setItem(localKey, this.data.index);
-  },
-});
-
 const init = (container) => {
-  new View({
+  const m = {
+    get: () => {
+      return parseInt(localStorage.getItem(localKey)) || 0;
+    },
+    set: (index) => {
+      localStorage.setItem(localKey, index);
+    },
+  };
+  new Vue({
     el: container,
-    data: m.data,
-    events: {
-      "click #tabBar > li": "toggle",
+    data: {
+      index: m.get(),
     },
-    html(index) {
-      return `
-    <div class="wrapper">
-      <ol id="tabBar">
-        <li class="${
-          index === 0 ? "selected" : ""
-        }" data-index="0"><span>item1</span></li>
-        <li class="${
-          index === 1 ? "selected" : ""
-        }" data-index="1"><span>item2</span></li>
-      </ol>
-      <ol id="tabContent">
-        <li class=${index === 0 ? "active" : ""}>111111</li>
-        <li class=${index === 1 ? "active" : ""}>222222</li>
-      </ol>
-    </div>
-    `;
-    },
-    render(data) {
-      const index = data.index;
-      if (this.el.children().length !== 0) this.el.empty();
-      $(this.html(index)).appendTo(this.el);
-    },
-    autoBindEvents() {
-      for (let key in this.events) {
-        const value = this[this.events[key]];
-        const spaceIndex = key.indexOf(" ");
-        const eventType = key.slice(0, spaceIndex);
-        const selector = key.substring(spaceIndex + 1);
-        this.el.on(eventType, selector, value);
-      }
-    },
-    toggle(e) {
-      const index = parseInt(e.currentTarget.dataset.index);
-      m.update({ index: index });
+    template: `
+      <section id="app2">
+      <div class="wrapper">
+        <ol id="tabBar">
+          <li :class="index === 0 ? 'selected' : ''" 
+              @click="index = 0"><span>item1</span></li>
+          <li :class="index === 1 ? 'selected' : ''" 
+              @click="index = 1"><span>item2</span></li>
+        </ol>
+        <ol id="tabContent">
+          <li :class="index === 0 ? 'active' : ''">111111</li>
+          <li :class="index === 1 ? 'active' : ''">222222</li>
+        </ol>
+      </div>
+      </section>
+    `,
+    watch: {
+      index() {
+        m.set(this.index);
+      },
     },
   });
 };
